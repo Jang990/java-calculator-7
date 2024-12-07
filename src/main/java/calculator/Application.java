7c3fc1b810ea36d0cd247c4ed8dff8fe8e3c0964
Jang <sdsd090811@gmail.com>
2024-12-07 15:36:19 +0900

    feat: Application에 기능 구현 코드 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,36 @@
 package calculator;
 
+import calculator.delimiter.DelimiterReader;
+import calculator.io.InputReader;
+import calculator.io.OutputPrinter;
+import calculator.numbers.PositiveNumbersReader;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        myCalculatorApp().run();
+    }
+
+    public static InputReader inputReader() {
+        return new InputReader();
+    }
+
+    public static OutputPrinter outputPrinter() {
+        return new OutputPrinter();
+    }
+
+    public static DelimiterReader delimiterReader() {
+        return new DelimiterReader();
+    }
+
+    public static PositiveNumbersReader positiveNumberReader() {
+        return new PositiveNumbersReader();
+    }
+
+    public static Calculator calculator() {
+        return new Calculator(delimiterReader(), positiveNumberReader());
+    }
+
+    public static MyCalculatorApp myCalculatorApp() {
+        return new MyCalculatorApp(inputReader(), outputPrinter(), calculator());
     }
 }
